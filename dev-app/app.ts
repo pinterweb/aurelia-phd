c4ec510f0347849caead9f59daa6196c37d8b8d7
Jeremy Zagorski <jmzagorski@gmail.com>
2019-12-03 10:07:40 -0500

    refactor(devapp): Remove debug logic

--- dev-app/app.ts
+++ dev-app/app.ts
@@ -40,27 +40,22 @@ export class App {
   constructor(private _options) {}
 
   activate(): void {
-    setTimeout(() => {
-      const _items = [];
-      for (let i = 0; i < 500; i++) {
-        const randomYear = 2000 + Math.floor(Math.random() * 10);
-        const randomMonth = Math.floor(Math.random() * 11);
-        const randomDay = Math.floor(Math.random() * 29);
-        const randomPercent = Math.round(Math.random() * 100);
+    for (let i = 0; i < 500; i++) {
+      const randomYear = 2000 + Math.floor(Math.random() * 10);
+      const randomMonth = Math.floor(Math.random() * 11);
+      const randomDay = Math.floor(Math.random() * 29);
+      const randomPercent = Math.round(Math.random() * 100);
 
-        _items[i] = {
-          id: i,
-          title: "Task " + i,
-          duration: Math.round(Math.random() * 100) + "",
-          percentComplete: randomPercent,
-          start: i === 465 ? "1990-01-01" : `${randomYear}-${randomMonth}-${randomDay}`,
-          finish: `${randomYear}-${randomMonth}-${randomDay}`,
-          effortDriven: i % 5 === 0
-        };
-      }
-
-      this.allItems = _items;
-    }, 2000);
+      this.allItems[i] = {
+        id: i,
+        title: "Task " + i,
+        duration: Math.round(Math.random() * 100) + "",
+        percentComplete: randomPercent,
+        start: `${randomYear}-${randomMonth}-${randomDay}`,
+        finish: `${randomYear}-${randomMonth}-${randomDay}`,
+        effortDriven: i % 5 === 0
+      };
+    }
   }
 
   changeFramework(framework: string): void {
